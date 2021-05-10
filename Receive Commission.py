f3c637dada1c292421d90b3366f50204f13f2035
Sebastian <38714137+Sebasti0n@users.noreply.github.com>
2021-05-10 12:48:58 -0500

    Update Receive Commission.py

--- Receive Commission.py
+++ Receive Commission.py
@@ -16,17 +16,26 @@ class stockbroker:
 
 # Creates a view of stocks and cryrpots from the perspective of the broker
     def user_profile (self, name): 
-        self.show_list_of_stocks_invested = []
-        self.display_cryptos_invested = []
-        self.get_commission = commission
-        self.stockbroker = name
+        self.list_of_stocks_invested = []  #array for stocks invested
+        self.list_cryptos_invested = []      #array for cryptos invseted     
+        self.get_commission = commission        #commission amount
+        self.stockbroker = name                 #user name for actor - broker 
+
+# Shows the amount invested by the client
+print ("The stocks the client ivested in: ")
+    for i in range(len(self.list_of_stocks_invested)):
+        print(self.list_of_stocks_invested[i], "\n")
+
+#displays the crypto currency the cliented invested in
+print ("The crypto currencies that the client invested in: ")
+    for i in range(len(self.list_of_cryptos_ivested)):
+        print(self.list_of_cryptos_invested[i], "\n")
 
 # Calculates the commission from stocks and cryptos that client invested in
     def calculate_commission (self, stocks, cryptos): 
-        self.commission_from_stocks = stocks * commission_rate
+        self.commission_from_stocks = total_invested * commission_rate
         self.commission_from_cryptos = cryptos * comission_rate
+        self.comission_total = commission_from_stocks + commission_from_cryptos 
 
-#Displays the comission based on the calculations
-    def display_comission_from_stocks = stocks
-
+print ("Your net commission amount : $", comission_total) #total commission recieved
     print ("Your comission is: ", #total)
