d32c17a896d61f7625a82f08dd3814c4d8187a06
cantuc40 <44282951+cantuc40@users.noreply.github.com>
2021-05-12 04:56:24 +0000

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,11 +1,17 @@
-# -*- coding: utf-8 -*-
-
+"""
+File Name: wallet
+Author: Christian Cantu 
+Purpose: Stores the users currency for purchasing stocks and cryptos
+in.
+"""
 
 class wallet:
     
     def __init__(self):
         self.funds = 0
         
+    def getFunds(self):
+        return self.funds
         
     def deposit(self, amount_of_cash):
         self.funds += amount_of_cash
